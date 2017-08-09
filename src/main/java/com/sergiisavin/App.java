3d3e741c6ddff40257b6ee0f928aff28057481c0
Sergii Savin <nissserge@gmail.com>
2017-08-09 23:14:27 +0200

    make Jsoup dependency compile scoped

--- src/main/java/com/sergiisavin/App.java
+++ src/main/java/com/sergiisavin/App.java
@@ -1,13 +1,39 @@
 package com.sergiisavin;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.sergiisavin.domainlinks.DomainLinks;
+import com.sergiisavin.htmlprocessor.HTMLProcessor;
+import com.sergiisavin.htmlprocessor.HTMLProcessorJsoup;
+import com.sergiisavin.linkscounter.LinksCounter;
+import com.sergiisavin.linkscounter.LinksCounterJsoup;
+import com.sergiisavin.linksextractor.LinksExtractor;
+import com.sergiisavin.linksextractor.LinksExtractorJsoup;
+import com.sergiisavin.pageloader.PageLoader;
+import com.sergiisavin.pageloader.PageLoaderJsoup;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        PageLoader loader = new PageLoaderJsoup();
+        LinksExtractor extractor = new LinksExtractorJsoup();
+        HTMLProcessor processor = new HTMLProcessorJsoup();
+        LinksCounter counter = new LinksCounterJsoup();
+
+        System.out.println("enter valid URL including protocol: ");
+        Scanner scanner = new Scanner(System.in);
+        String url = scanner.nextLine();
+
+        String html = loader.loadPage(url);
+        Set<String> links = extractor.extractLinks(html);
+        Map<String, DomainLinks> domainLinks = processor.process(links);
+        Map<String, Integer> countedLinks = counter.countLinks(domainLinks);
+
+        for(String link : countedLinks.keySet()){
+            System.out.println(link + " " + countedLinks.get(link));
+        }
+
     }
 }
--- src/test/java/com/sergiisavin/LinksExtractorTest.java
+++ src/test/java/com/sergiisavin/LinksExtractorTest.java
@@ -2,6 +2,7 @@ package com.sergiisavin;
 
 import com.sergiisavin.linksextractor.LinksExtractor;
 import com.sergiisavin.linksextractor.LinksExtractorJsoup;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
@@ -11,12 +12,30 @@ import static org.junit.Assert.assertTrue;
 
 public class LinksExtractorTest {
 
-    String page = "<html><head></head><body><a href=\"site1.com\">link</a></body></html>";
+    String pageWith1Link = "<html><head></head><body>" +
+            "<a href='http://www.site1.com'>link</a></body></html>";
+    String pageWith2Links = "<html><head></head><body>" +
+            "<a href='http://www.site1.com'>link1</a>" +
+            "<a href='http://www.site2.org'>link2</a></body></html>";
+    LinksExtractor extractor;
+
+    @Before
+    public void setUp() throws Exception {
+        extractor = new LinksExtractorJsoup();
+    }
+
     @Test
     public void givenPageWithOneLinkExtractTheLink() throws Exception {
-        LinksExtractor extractor = new LinksExtractorJsoup();
-        Set<String> links = extractor.extractLinks(page);
+        Set<String> links = extractor.extractLinks(pageWith1Link);
         assertEquals(1, links.size());
-        assertTrue(links.contains("site1.com"));
+        assertTrue(links.contains("http://www.site1.com"));
+    }
+
+    @Test
+    public void givenTwoLinksExtract2Links() throws Exception {
+        Set<String> links = extractor.extractLinks(pageWith2Links);
+        assertEquals(2, links.size());
+        assertTrue(links.contains("http://www.site1.com"));
+        assertTrue(links.contains("http://www.site2.org"));
     }
 }
